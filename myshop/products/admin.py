9dc217b503990915e25302d88ce823ece90999ca
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-12-20 15:18:10 +0000

    ggggff

--- myshop/products/admin.py
+++ myshop/products/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
 
+admin.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
